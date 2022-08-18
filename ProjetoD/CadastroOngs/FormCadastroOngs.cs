b8fb19d7fb732fc60eca47563b794f17f0bd03b4
Andrielly Oliveira <andriellyrodrigues.rs@gmail.com>
2022-08-17 22:45:31 -0300

    tela cadastro das ongs

--- ProjetoD/CadastroOngs/FormCadastroOngs.cs
+++ ProjetoD/CadastroOngs/FormCadastroOngs.cs
@@ -7,14 +7,5 @@ namespace CadastroOngs
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
