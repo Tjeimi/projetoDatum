5ae234272d916f64b25eca34a9fc2c8e4a714540
R <yoiwappl@gmail.com>
2022-08-31 22:39:38 -0300

    adicionado utf8 no initdb do postgresql

--- ProjetoD/ServerBin/PostgreSQL/14/StartDB.bat
+++ ProjetoD/ServerBin/PostgreSQL/14/StartDB.bat
@@ -5,5 +5,5 @@
 @SET PGUSER=postgres
 @SET PGPORT=5434
 @SET PGLOCALEDIR=%~dp0\share\locale
-"%~dp0\bin\initdb" -U postgres -A trust
+"%~dp0\bin\initdb" -U postgres -A trust -E 'UTF-8' --lc-collate=pt_BR.UTF-8
 "%~dp0\bin\pg_ctl" -D "%~dp0/data" -l logfile start
